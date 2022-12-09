28c85d317055040f5100e67560aadb7022b2cca8
Fernando Cristovão <fe.acristovao@gmail.com>
2022-12-08 21:58:20 -0300

    Criado task para atualizar o banco

---
+++ api/db/seed.rb
@@ -0,0 +1,19 @@
+Dir[File.expand_path(File.join(__dir__, '..', 'app', 'models', '*.rb'))].each { |f| require f }
+require 'rest-client'
+require 'json'
+
+url = 'https://api.coinbase.com/v2/exchange-rates?currency=USD'
+request = RestClient.get(url)
+response = JSON.parse(request)
+
+unless response.nil?
+
+  data = response['data']['rates']
+  data.each do |currency, rate|
+    new_currency = Currency.find(name: currency) || Currency.new
+    new_currency.name = currency
+    new_currency.ratio = rate
+    new_currency.is_virtual = false
+    new_currency.save
+  end
+end
